9b18fd07c6b487fbdbaea5c7f0cbca16ae71407d
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-02-02 11:08:09 -0600

    Updating credits

--- src/Scenes/CreditsScene.js
+++ src/Scenes/CreditsScene.js
@@ -7,7 +7,7 @@ export default class CreditsScene extends Phaser.Scene {
 
   create () {
     this.creditsText = this.add.text(0, 0, 'Credits', { fontSize: '32px', fill: '#fff' });
-    this.madeByText = this.add.text(0, 0, 'Created By: Placeholder', { fontSize: '26px', fill: '#fff' });
+    this.madeByText = this.add.text(0, 0, 'Created By: Houda Cherkaoui', { fontSize: '26px', fill: '#a8dadc' });
     this.zone = this.add.zone(config.width/2, config.height/2, config.width, config.height);
 
     Phaser.Display.Align.In.Center(
