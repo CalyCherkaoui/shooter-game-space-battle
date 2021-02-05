2e5b3adf91e5f9619099d6d8a9a827ffd5173261
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-02-05 08:07:32 -0600

    Adding player class that extend entity

--- src/Objects/Player.js
+++ src/Objects/Player.js
@@ -4,6 +4,23 @@ import Entity from './Entity';
 export default class Player extends Entity {
   constructor(scene, x, y, key) {
     super(scene, x, y, key, 'Player');
+    this.setData('speed', 200);
+    this.play("sprPlayer");
+  }
 
+  moveUp() {
+    this.body.velocity.y = -this.getData("speed");
+  }
+  
+  moveDown() {
+    this.body.velocity.y = this.getData("speed");
+  }
+  
+  moveLeft() {
+    this.body.velocity.x = -this.getData("speed");
+  }
+  
+  moveRight() {
+    this.body.velocity.x = this.getData("speed");
   }
 }
