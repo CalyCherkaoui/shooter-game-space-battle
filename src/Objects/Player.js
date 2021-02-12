b4d6ff5f2d817270f3ba520b8e7ece8726009252
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-02-11 18:03:52 -0600

    Adding on destroy and update methods

--- src/Objects/Player.js
+++ src/Objects/Player.js
@@ -22,6 +22,7 @@ export default class Player extends Entity {
     this.setData("isShooting", false);
     this.setData("timerShootDelay", 10);
     this.setData("timerShootTick", this.getData("timerShootDelay") - 1);
+    this.play("spaceJet"); // play the player sprite animation
   }
 
   moveUp() {
@@ -56,5 +57,18 @@ export default class Player extends Entity {
     this.body.setVelocity(0, 0);
     this.x = Phaser.Math.Clamp(this.x, 0, this.scene.game.config.width);
     this.y = Phaser.Math.Clamp(this.y, 0, this.scene.game.config.height);
+
+    if (this.getData("isShooting")) {
+      if (this.getData("timerShootTick") < this.getData("timerShootDelay")) {
+        this.setData("timerShootTick", this.getData("timerShootTick") + 1); // every game update, increase timerShootTick by one until we reach the value of timerShootDelay
+      }
+      else { // when the "manual timer" is triggered:
+        var laser = new laserWepon(this.scene, this.x, this.y);
+        this.scene.playerLasers.add(laser);
+      
+        this.scene.sfx.laser.play(); // play the laser sound effect
+        this.setData("timerShootTick", 0);
+      }
+    }
   }
 }
