32966a948c1f4ac40b4af5e0c75c246caad47503
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-02-26 16:35:34 -0600

    Adding Api module

--- src/Objects/api.js
+++ src/Objects/api.js
@@ -1,37 +1,38 @@
 import 'regenerator-runtime';
 
-const key = 'ObtnnnA6KkTqmbDXfbxz';
-const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${key}/scores/ `;
-
 const fetch = require('node-fetch');
 
-const writeScore = async (userLogin, score) => {
-  const scoreData = { userLogin, score };
-  const data = {
-    method: 'POST',
-    mode: 'cors',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(scoreData),
+const api = (() => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ObtnnnA6KkTqmbDXfbxz/scores/';
+  const writeScore = async (userLogin, score) => {
+    const scoreData = { user: userLogin, score: Number(score) };
+    const data = {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(scoreData),
+    };
+    const response = await fetch(url, data);
+    const result = await response.json();
+    return result;
   };
-  const response = await fetch(url, data);
-  const result = await response.json();
-  return result;
-};
-
-const readScore = async () => {
-  const data = {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
+  const readScores = async () => {
+    const data = {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(url, data);
+    const scores = await response.json();
+    return scores;
   };
-  const response = await fetch(url, data);
-  const scores = await response.json();
-  return scores.result;
-};
 
-export { writeScore, readScore };
+  return { writeScore, readScores };
+})();
+
+export default api;
