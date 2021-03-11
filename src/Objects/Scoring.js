3be12eff00e6122af0aca51c348e329102c82f1c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-03-11 10:01:29 -0600

    fixing linter error

--- src/Objects/Scoring.js
+++ src/Objects/Scoring.js
@@ -2,7 +2,6 @@ import Phaser from 'phaser';
 
 export default class Scoring extends Phaser.GameObjects.Text {
   constructor(scene, x, y, score, style) {
-    // const scoreText = `Your Score: ${score}`;
     super(scene, x, y, score, style);
 
     this.score = score;
